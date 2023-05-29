0737c982053b2079f2b0fb015dcd979dc1fcfdb6
Hossein Sharifi <47387238+sharifi98@users.noreply.github.com>
2023-05-29 17:51:50 +0200

    Initial Commit

---
+++ Karriaredagen/KarriaredagenApp.swift
@@ -0,0 +1,17 @@
+//
+//  KarriaredagenApp.swift
+//  Karriaredagen
+//
+//  Created by Hossein Sharifi on 29/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct KarriaredagenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
