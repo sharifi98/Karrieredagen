5507508fa9ba775047c85c3ef6bf4bd8f4dc7fb5
Hossein Sharifi <47387238+sharifi98@users.noreply.github.com>
2023-06-08 09:51:04 +0200

    added a back button in the schedule detail which dismisses the sheet and goes back to the schedule view

--- Karriaredagen/Views/Schedule/ScheduleDetail.swift
+++ Karriaredagen/Views/Schedule/ScheduleDetail.swift
@@ -9,59 +9,81 @@ import SwiftUI
 
 struct EventDetailView: View {
     var event: Event
-
+    
     var body: some View {
-        ScrollView {
-            VStack(alignment: .leading, spacing: 10) {
-                event.image
-                    .resizable()
-                    .scaledToFit()
-                
-                Text(event.name)
-                    .font(.title)
-
-                Text("\(event.start_time) - \(event.end_time)")
-                    .font(.subheadline)
-
-                Text(event.location)
-                    .font(.subheadline)
-                
-                Spacer()
-
-                Group {
-                    Text("Speakers:")
-                        .font(.headline)
-                    if let speakers = event.speakers {
-                        ForEach(speakers, id: \.self) { speaker in
-                            Text(speaker)
-                        }
-                    } else {
-                        Text("No speakers listed.")
+        VStack(alignment: .leading, spacing: 10) {
+            event.image
+                .resizable()
+                .scaledToFit()
+            
+            Text(event.name)
+                .font(.title)
+            
+            Text("\(event.start_time) - \(event.end_time)")
+                .font(.subheadline)
+            
+            Text(event.location)
+                .font(.subheadline)
+            
+            
+            Group {
+                Text("Speakers:")
+                    .font(.headline)
+                if let speakers = event.speakers {
+                    ForEach(speakers, id: \.self) { speaker in
+                        Text(speaker)
                     }
+                } else {
+                    Text("No speakers listed.")
                 }
-
-                Spacer()
-
-                Group {
-                    Text("Companies:")
-                        .font(.headline)
-                    if let companies = event.companies {
-                        ForEach(companies, id: \.self) { company in
-                            Text(company)
-                        }
-                    } else {
-                        Text("No companies listed.")
-                    }
-                }
-                
             }
-            .padding()
+            
+            
+            Group {
+                Text("Companies:")
+                    .font(.headline)
+                if let companies = event.companies {
+                    ForEach(companies, id: \.self) { company in
+                        Text(company)
+                    }
+                } else {
+                    Text("No companies listed.")
+                }
+            }
+            
+            Spacer()
+            
+            HStack(alignment: .center) {
+                Spacer()
+                ExtractedView()
+                Spacer()
+            }
+            
         }
+        .padding()
+        
     }
 }
 
 struct EventDetailView_Previews: PreviewProvider {
     static var previews: some View {
         EventDetailView(event: events[0])
+    }
+}
+
+struct ExtractedView: View {
+    @Environment(\.dismiss) var dismiss
+    
+    var body: some View {
+        Button {
+            dismiss()
+        } label: {
+            Text("Tilbake")
+        }
+        .padding(10)
+        .foregroundColor(.white)
+        .clipShape(Rectangle())
+        .background(.blue)
+        .cornerRadius(15)
     }
 }
