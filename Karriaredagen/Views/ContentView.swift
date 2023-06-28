d91f23a140358045c48485db80acf8634813f0de
Hossein Sharifi <47387238+sharifi98@users.noreply.github.com>
2023-06-28 18:52:03 +0200

    removed the tab view

--- Karriaredagen/Views/ContentView.swift
+++ Karriaredagen/Views/ContentView.swift
@@ -4,25 +4,30 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        TabView {
-            KDView()
-                .tabItem {
-                    Label("KD", systemImage: "graduationcap")
-                }
-            ScheduleList()
-                .tabItem {
-                    Label("Timeplan", systemImage: "calendar")
-                }
-            GriegView()
-                .tabItem {
-                    Label("Kart", systemImage: "map")
-                }
-            OmOssView()
-                .tabItem {
-                    Label("Om oss", systemImage: "ellipsis.circle")
-                }
-        }
-        .accentColor(.orange) // Set the tab item's text and icon color
+        KDView()
+        
+        /* TabView {
+         KDView()
+         .tabItem {
+         Label("KD", systemImage: "graduationcap")
+         }
+         ScheduleList()
+         .tabItem {
+         Label("Timeplan", systemImage: "calendar")
+         }
+         GriegView()
+         .tabItem {
+         Label("Kart", systemImage: "map")
+         }
+         OmOssView()
+         .tabItem {
+         Label("Om oss", systemImage: "ellipsis.circle")
+         }
+         }
+         .accentColor(.orange) // Set the tab item's text and icon color
+         */
+        
+        
         
     }
 }
--- Karriaredagen/Views/OmOss/OmOssView.swift
+++ Karriaredagen/Views/OmOss/OmOssView.swift
@@ -11,52 +11,99 @@ struct OmOssView: View {
     var body: some View {
         NavigationView {
             List {
-                NavigationLink(destination: KarrieredagenInformationView()) {
-                    Label {
-                        Text("Karrieredagen")
-                    } icon: {
-                        Image(systemName: "network")
-                            .foregroundColor(.blue)
+                Section {
+                    NavigationLink(destination: KarrieredagenInformationView()) {
+                        Label {
+                            Text("Karrieredagen")
+                        } icon: {
+                            Image(systemName: "network")
+                                .foregroundColor(.blue)
+                        }
                     }
                     
-                }
-                
-                NavigationLink(destination: SpringbrettfondetView()) {
+                    NavigationLink(destination: SpringbrettfondetView()) {
 
-                    Label {
-                        Text("Springbrettfondet")
-                    } icon: {
-                        Image(systemName: "chart.line.uptrend.xyaxis")
-                            .foregroundColor(.green)
+                        Label {
+                            Text("Springbrettfondet")
+                            
+                        } icon: {
+                            Image(systemName: "chart.line.uptrend.xyaxis")
+                                .foregroundColor(.green)
+                        }
+                        
+                    }
+                    
+                    NavigationLink(destination: ProsjekterView()) {
+                        
+                        Label {
+                            Text("Prosjekter")
+                        } icon: {
+                            Image(systemName: "hammer")
+                                .foregroundColor(.purple)
+                        }
+                    }
+                    
+                    NavigationLink(destination: HvemErSpringbrettetView()) {
+                        
+                        Label {
+                            Text("Om Oss")
+                        } icon: {
+                            Image(systemName: "info.circle")
+                                .foregroundColor(.gray)
+                        }
+
                     }
                     
                 }
                 
-                NavigationLink(destination: ProsjekterView()) {
-                    
-                    Label {
-                        Text("Prosjekter")
-                    } icon: {
-                        Image(systemName: "hammer")
-                            .foregroundColor(.purple)
-                    }
-                }
                 
-                NavigationLink(destination: HvemErSpringbrettetView()) {
-                    
-                    Label {
-                        Text("Om Oss")
-                    } icon: {
-                        Image(systemName: "info.circle")
-                            .foregroundColor(.gray)
+                Section {
+                    Button(action: {
+                        if let url = URL(string: "https://www.facebook.com/springbrettet/") {
+                            UIApplication.shared.open(url)
+                        }
+                    }) {
+                        HStack {
+                            Image(systemName: "person")
+                            Text("Facebook")
+                        }
+                        .foregroundColor(.blue)
                     }
 
+                    Button(action: {
+                        if let url = URL(string: "https://www.instagram.com/springbrettet/") {
+                            UIApplication.shared.open(url)
+                        }
+                    }) {
+                        HStack {
+                            Image(systemName: "camera")
+                            Text("Instagram")
+                        }
+                        .foregroundColor(.pink)
+                    }
+
+                    Button(action: {
+                        if let url = URL(string: "https://www.linkedin.com/company/springbrettet/mycompany/") {
+                            UIApplication.shared.open(url)
+                        }
+                    }) {
+                        HStack {
+                            Image(systemName: "point.3.filled.connected.trianglepath.dotted")
+                            Text("LinkedIn")
+                        }
+                        .foregroundColor(.cyan)
+                    }
                 }
+
+                
+
             }
             //.foregroundColor(Color(red: 0.945, green: 0.529, blue: 0.196))
             .foregroundColor(.orange)
             //.navigationTitle("Om Oss")
             .listStyle(.grouped)
+            
+
         }
     }
 }
