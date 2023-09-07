c58973bbb532a3f4e9b9b9b77d9eb3b1015024b1
Hossein Sharifi <47387238+sharifi98@users.noreply.github.com>
2023-09-07 14:33:47 +0200

    rehauld the background of personview and added a hosseinsharifiview

---
+++ Springbrettet/Views/OmOss/Om oss/HosseinSharifiView.swift
@@ -0,0 +1,18 @@
+//
+//  HosseinSharifiView.swift
+//  Springbrettet
+//
+//  Created by Hossein Sharifi on 07/09/2023.
+//
+
+import SwiftUI
+
+struct HosseinSharifiView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HosseinSharifiView()
+}
