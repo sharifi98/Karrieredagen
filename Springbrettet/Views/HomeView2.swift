e308a8f697546be33cb9d71e0b40a784853e63d6
Hossein Sharifi <47387238+sharifi98@users.noreply.github.com>
2023-11-10 11:46:30 +0100

    started refactoring the HomeView, created a HomeView2 to test.

---
+++ Springbrettet/Views/HomeView2.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView2.swift
+//  Springbrettet
+//
+//  Created by Hossein Sharifi on 10/11/2023.
+//
+
+import SwiftUI
+
+struct HomeView2: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomeView2()
+}
